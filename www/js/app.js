c464e146e99ba3ad9d101ca53d4061cc494a923b
Stone C. Lasley <stonelasley@gmail.com>
2016-02-21 16:12:53 -0700

    add task functionality

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,39 @@
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
+
+  .controller('TodoCtrl', function ($scope, $ionicModal) {
+
+    $scope.tasks = [
+      {title: 'Collect coins'},
+      {title: 'Eat Mushrooms'},
+      {title: 'Get high enough to grab the flag'},
+      {title: 'Find the Princess'}
+    ];
+
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    $scope.createTask = function (task) {
+
+      $scope.tasks.push({
+        title: task.title
+      });
+
+      $scope.taskModal.hide();
+      task.title = '';
+    };
+
+    $scope.newTask = function () {
+
+      $scope.taskModal.show();
+    };
+
+    $scope.closeNewTask = function () {
+
+      $scope.taskModal.hide();
+    };
+  });
